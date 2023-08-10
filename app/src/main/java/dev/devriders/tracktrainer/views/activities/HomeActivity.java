b2bb76619496a2236ccd773ba4d2221f2eef67f3
MrPotatoXx <salinasgiovanni25@gmail.com>
2023-08-09 23:59:07 -0400

    Se instancian los fragmnts y se cierra sesion

--- app/src/main/java/dev/devriders/tracktrainer/views/activities/HomeActivity.java
+++ app/src/main/java/dev/devriders/tracktrainer/views/activities/HomeActivity.java
@@ -1,16 +1,79 @@
 package dev.devriders.tracktrainer.views.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import dev.devriders.tracktrainer.R;
+import dev.devriders.tracktrainer.views.fragments.EjerciciosFragment;
+import dev.devriders.tracktrainer.views.fragments.MisionesFragment;
+import dev.devriders.tracktrainer.views.fragments.UsuarioFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private EjerciciosFragment ejerciciosFragment;
+    private MisionesFragment misionesFragment;
+    private UsuarioFragment usuarioFragment;
+
+    private static final int MENU_EJERCICIOS = R.id.nav_ejercicios;
+    private static final int MENU_MISIONES = R.id.nav_misiones;
+    private static final int MENU_PERFIL = R.id.nav_perfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ejerciciosFragment = new EjerciciosFragment();
+        misionesFragment = new MisionesFragment();
+        usuarioFragment = new UsuarioFragment();
+
+        // Mostrar el fragmento de ejercicios por defecto
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, ejerciciosFragment)
+                .commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == MENU_EJERCICIOS) {
+                showEjerciciosFragment();
+                return true;
+            } else if (itemId == MENU_MISIONES) {
+                showMisionesFragment();
+                return true;
+            } else if (itemId == MENU_PERFIL) {
+                showUsuarioFragment();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void showEjerciciosFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, ejerciciosFragment)
+                .commit();
+    }
+
+    private void showMisionesFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, misionesFragment)
+                .commit();
+    }
+
+    private void showUsuarioFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, usuarioFragment)
+                .commit();
     }
 }
--- app/src/main/java/dev/devriders/tracktrainer/views/fragments/UsuarioFragment.java
+++ app/src/main/java/dev/devriders/tracktrainer/views/fragments/UsuarioFragment.java
@@ -1,66 +1,74 @@
 package dev.devriders.tracktrainer.views.fragments;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import dev.devriders.tracktrainer.R;
+import dev.devriders.tracktrainer.views.activities.welcome.WelcomeActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsuarioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UsuarioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public UsuarioFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsuarioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UsuarioFragment newInstance(String param1, String param2) {
-        UsuarioFragment fragment = new UsuarioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView listView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_usuario, container, false);
+
+        listView = view.findViewById(R.id.optionsListView);
+        String[] options = {"Ver perfil", "Términos y condiciones", "Cerrar sesión o salir"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        // TODO: Ir a la pantalla del perfil
+                        // Intent intentProfile = new Intent(getContext(), ProfileActivity.class);
+                        // startActivity(intentProfile);
+                        break;
+                    case 1:
+                        // TODO: Ir a la pantalla de términos y condiciones
+                        // Intent intentTerms = new Intent(getContext(), TermsConditionsActivity.class);
+                        // startActivity(intentTerms);
+                        break;
+                    case 2:
+                        cerrarSesionYSalir();
+                        break;
+                }
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false);
+    private void cerrarSesionYSalir() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("token");
+        editor.apply();
+        Intent intent = new Intent(getContext(), WelcomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
